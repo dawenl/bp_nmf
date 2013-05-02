67d058e48ed6edc696d855aa99a9c3387e73462b
Dawen Liang <dl2771@columbia.edu>
2013-05-02 05:00:40 -0400

    Experiments

--- BP_Eval.py
+++ BP_Eval.py
@@ -29,20 +29,23 @@ X[X < 1e-8 * x_max] = 1e-8 * x_max
 
 # <codecell>
 
-def train(X, K, init_option, alpha, N, timed):
+def train(X, K, init_option, alpha, N, timed, objs=None):
     bnmf = bp_vbayes.Bp_NMF(X, K=K, init_option=init_option, alpha=alpha)
     for n in xrange(N):
         start_t = time.time()
         ind = bnmf.update(timed=timed[n])
         if ind == -1:
-            if n == 0:
+            if n == 0 or n == 1:
                 # the initialization can be bad and the first iteration will suck, so restart
-                print 'bad init, restart'
+                print '***Bad initial values, restart***'
                 return train(X, K, init_option, alpha, N, timed)
             else:
+                # this should rarely happen
                 print 'oops'
                 sys.exit(-1)
         t = time.time() - start_t
+        if objs is not None:
+            objs[n] = bnmf.obj
         print 'Dictionary Learning: Iteration: {}, good K: {}, time: {:.2f}'.format(n, bnmf.good_k.shape[0], t)
     return bnmf
 
@@ -58,6 +61,10 @@ def encode(bnmf, X, K, ED, ED2, init_option, alpha, N, timed):
         print 'Encoding: Iteration: {}, good K: {}, time: {:.2f}'.format(n, bnmf.good_k.shape[0], t)
     return bnmf
 
+# <headingcell level=1>
+
+# Band expansion experiemnt for time-dependent BP-NMF
+
 # <codecell>
 
 '''
@@ -66,8 +73,7 @@ Band expansion for time-dependent BP-NMF
 #reload(bp_vbayes)
 
 # Take the middle 4000 frames for cross-validation
-F = X.shape[0]
-L = X.shape[1]
+F, L = X.shape
 D = X[:, (L-4000)/2.:(L+4000)/2.]
 # Model/training parameters
 K = 512
@@ -76,8 +82,8 @@ alpha = 2.
 N = 20
 timed = ones((N,), dtype='bool')
 timed[0] = False
-#score = []
-#good_ks = []
+score = []
+good_ks = []
 for i in xrange(2, 5):
     print i
     idx = ones((4000,), dtype='bool')
@@ -93,7 +99,11 @@ for i in xrange(2, 5):
     good_ks.append(bnmf.good_k.shape[0])
 print good_ks
 print score
-sio.savemat('bpt_nmf_K{}_pinkmoon'.format(K), {'goodks':good_ks, 'score':score})
+sio.savemat('bpnmf_PinkMoon_1N19T_Scale.mat', {'goodks':good_ks, 'score':score})
+
+# <headingcell level=1>
+
+# Band expansion experiment for i.i.d. BP-NMF
 
 # <codecell>
 
@@ -101,8 +111,7 @@ sio.savemat('bpt_nmf_K{}_pinkmoon'.format(K), {'goodks':good_ks, 'score':score})
 Band expansion for i.i.d. BP-NMF
 '''
 # Take the middle 4000 frames for cross-validation
-F = X.shape[0]
-L = X.shape[1]
+F, L = X.shape
 D = X[:, (L-4000)/2.:(L+4000)/2.]
 # Model/training parameters
 K = 512
@@ -110,9 +119,9 @@ init_option = 'Rand'
 alpha = 2.
 N = 20
 timed = zeros((N,), dtype='bool')
-score = []
-good_ks = []
-for i in xrange(5):
+#score = []
+#good_ks = []
+for i in [2,4]:
     print i
     idx = ones((4000,), dtype='bool')
     idx[i*800:(i+1)*800] = False
@@ -123,14 +132,17 @@ for i in xrange(5):
     bnmf = encode(bnmf, D_test[:F-192,:], Dict.shape[1], Dict[:F-192,:], Dict2[:F-192,:], init_option, alpha, N, timed)
     X_pred = dot(Dict[:,bnmf.good_k], (bnmf.ES * around(bnmf.EZ))[bnmf.good_k,:])
     sigma = sqrt( mean( (D_test[:F-192,:] - X_pred[:F-192,:])**2 ) )
-    score.append(sum(scipy.stats.norm.logpdf(X_pred[:-192,:], loc=D_test[:-192,:], scale=sigma)))  
-    good_ks.append(bnmf.good_k.shape[0])
+    #score.append(sum(scipy.stats.norm.logpdf(X_pred[:-192,:], loc=D_test[:-192,:], scale=sigma)))  
+    #good_ks.append(bnmf.good_k.shape[0])
+    score[i] = sum(scipy.stats.norm.logpdf(X_pred[:-192,:], loc=D_test[:-192,:], scale=sigma))
+    good_ks[i] = bnmf.good_k.shape[0]
 print good_ks
 print score
-sio.savemat('bp_nmf_K{}_pinkmoon'.format(K), {'goodks':good_ks, 'score':score})
+sio.savemat('bpnmf_PinkMoon_20N_Scale.mat', {'goodks':good_ks, 'score':score})
 
 # <codecell>
 
+print good_ks, score
 
 # <codecell>
 
--- bp_nmf.py
+++ bp_nmf.py
@@ -3,146 +3,136 @@
 
 # <codecell>
 
-import time, pickle
+import time, pickle, functools
 import bp_vbayes
 from plot_utils import *
 import librosa
+import scipy.io as sio
 
 # <codecell>
 
+'''
 ## Load Pickled data if necessary
-filename = 'bnmf_mix5a_R_20T'
+filename = ''
 with open(filename, 'r') as output:
     bnmf = pickle.load(output)
+'''
 
 # <codecell>
 
-#x, sr = librosa.load('../data/mix_var5a_8k.wav', sr=None)
-#x, sr = librosa.load('../data/demo.wav')
-x, sr = librosa.load('../data/PinkMoon.mp3', sr=22050)
+specshow = functools.partial(imshow, cmap=cm.hot_r, aspect='auto', origin='lower', interpolation='nearest')
+
+def logspec(X, amin=1e-10, dbdown=80):
+    logX = 20*log10(np.maximum(X, amin))
+    return np.maximum(logX, logX.max() - dbdown)
+    
+## save as a Picked object
+def save_object(obj, filename):
+    with open(filename, 'wb') as output:
+        pickle.dump(obj, output, pickle.HIGHEST_PROTOCOL)
+
+# <codecell>
+
+def train(X, K, init_option, alpha, N, timed, objs=None):
+    bnmf = bp_vbayes.Bp_NMF(X, K=K, init_option=init_option, alpha=alpha)
+    for n in xrange(N):
+        start_t = time.time()
+        ind = bnmf.update(timed=timed[n])
+        if ind == -1:
+            if n == 0 or n == 1:
+                # the initialization can be bad and the first iteration will suck, so restart
+                print '***Bad initial values, restart***'
+                return train(X, K, init_option, alpha, N, timed)
+            else:
+                # this should rarely happen
+                print 'oops'
+                sys.exit(-1)
+        t = time.time() - start_t
+        if objs is not None:
+            objs[n] = bnmf.obj
+        print 'Dictionary Learning: Iteration: {}, good K: {}, time: {:.2f}'.format(n, bnmf.good_k.shape[0], t)
+    return bnmf
+
+# <codecell>
+
+def get_data(filename, n_fft, hop_length, reweight=True):
+    x, _ = librosa.load(filename)
+    X = np.abs(librosa.stft(x, n_fft=n_fft, hop_length=hop_length))
+    print X.shape
+    specshow(logspec(X))
+    if reweight:
+        std_col = np.maximum(np.sqrt(np.var(X, axis=1, keepdims=True)), 1e-10)
+    else:
+        std_col = 1.
+    X /= std_col
+    # cut off values 80db below maximum for numerical consideration
+    x_max = X.max()
+    X[ X < 1e-8 * x_max] = 1e-8 * x_max
+    return (X, std_col)
+
+# <codecell>
+
+#reload(bp_vbayes)
+filename = '../data/mix_var5a_22k.wav'
 n_fft = 512
 hop_length = 512
-Xc = librosa.stft(x, n_fft=n_fft, hop_length=hop_length)
-X = abs(Xc)
-K = 512
-print X.shape
-std_col = np.sqrt(np.var(X, axis=1, keepdims=True)) + 1e-6
-X /= std_col
-
-#X /= amax(X)
-#X[X < 1e-8] = 1e-8
-x_max = amax(X)
-X[ X < 1e-8 * x_max] = 1e-8 * x_max
+reweight = False
+X, std_col = get_data(filename, n_fft, hop_length, reweight=reweight)
 
 # <codecell>
 
-reload(bp_vbayes)
 init_option = 'Rand'
 alpha = 2.
-bnmf = bp_vbayes.Bp_NMF(X, K=K, init_option=init_option, RSeed=random.seed(98765), alpha=alpha)
-
-# <codecell>
-
+K = 512
 N = 20
 #timed = zeros((N,), dtype='bool')
 timed = ones((N,), dtype='bool')
 timed[0] = False
-#timed[-10:] = True
-obj = []
-for i in xrange(N):
-    start_t = time.time()
-    bnmf.update(timed=timed[i])
-    t = time.time() - start_t
-    print 'Iteration: {}, good K: {}, time: {:.2f}'.format(i,bnmf.good_k.shape[0], t)
-    obj.append(bnmf.obj)
+objs = empty((N,))
 
-# <codecell>
-
-threshold = 0.0001
-N = 30
-#timed = zeros((N,), dtype='bool')
-timed = ones((N,), dtype='bool')
-timed[0] = False
-#timed[-5:] = True
-obj = []
-improvement = 1
-for i in xrange(N):
-    start_t = time.time()
-    bnmf.update(timed=timed[i])
-    t = time.time() - start_t
-    
-    if i > 0:
-        last_score = score
-    score = bnmf.obj
-    obj.append(score)
-    if i > 0:
-        improvement = (score - last_score)/abs(last_score)
-    print 'Iteration: {}, good K: {}, time: {:.2f}, improvement: {:.4f}'.format(i, bnmf.good_k.shape[0], t, improvement)
-    if improvement < threshold:
-        break
+bnmf = train(X, K, init_option, alpha, N, timed, objs)
 
 # <codecell>
 
 print 'sigma_error = {}'.format(sqrt(1./bnmf.Eg))
 print diff(obj)
-plot(obj[1:])
+plot(obj)
 pass
 
 # <codecell>
 
 good_k = bnmf.good_k
-# Rescaling the data
-high_cut = 20 #db
-low_cut = -60 #db
-threshold_high = 10**(high_cut/20.)
-threshold_low = 10**(low_cut/20.)
 
-tmpX = X.copy()
-tmpX *= std_col
 Xres = dot(bnmf.ED, bnmf.ES * around(bnmf.EZ)) * std_col
-res = tmpX - Xres
-
-tmpX /= (np.max(tmpX)/threshold_high)
-Xres /= (np.max(Xres)/threshold_high)
-tmpX[tmpX < threshold_low] = threshold_low
-Xres[Xres < threshold_low] = threshold_low
+res = X*std_col - Xres
 
 ## Original v.s. Reconstruction
 figure(1)
-plot_decomp(args=({'D':20*log10(tmpX), 'T':'Original Spectrogram'}, 
-            {'D':20*log10(Xres), 'T':'Reconstruction '} , 
+plot_decomp(args=({'D':logspec(X*std_col), 'T':'Original Spectrogram'}, 
+            {'D':logspec(Xres), 'T':'Reconstruction '} , 
             {'D':res, 'T':'Reconstruction Error'}), cmap=cm.hot_r)
 ## Plot decomposition
 idx = flipud(argsort(bnmf.Epi[good_k]))
-tmpED = bnmf.ED.copy()
-tmpED /= (np.max(bnmf.ED, axis=0)/threshold_high)
-tmpES = bnmf.ES.copy()
-tmpES *= (np.max(bnmf.ED, axis=0, keepdims=True).T/threshold_high)
-tmpED[tmpED < threshold_low] = threshold_low
+tmpES = bnmf.ES[good_k[idx],:].copy()
+tmpES *= np.max(bnmf.ED[:,good_k[idx]], axis=0, keepdims=True).T
 figure(2)
-plot_decomp(args=({'D':20*log10(tmpED[:,good_k[idx]]), 'T':'ED'}, 
+plot_decomp(args=({'D':logspec(bnmf.ED[:,good_k[idx]]), 'T':'ED'}, 
             {'D':around(bnmf.EZ[good_k[idx],:]), 'T':'EZ'}, 
-            {'D':(around(bnmf.EZ)*tmpES)[good_k[idx],:], 'T':'ES*EZ'}), cmap=cm.hot_r)
+            {'D':around(bnmf.EZ[good_k[idx],:])*tmpES, 'T':'ES*EZ'}), cmap=cm.hot_r)
 figure(3)
 plot(flipud(sort(bnmf.Epi[good_k])), '-o')
 title('Expected membership prior Pi')
+pass
 
 # <codecell>
 
-bar(arange(std_col.shape[0]), std_col)
-
-# <codecell>
-
-tmpED = bnmf.ED.copy()
+tmpED = bnmf.ED[:,good_k[idx]].copy()
 tmpED /= np.sum(tmpED**2, axis=0)**0.5
-plot_decomp(args=(dot(tmpED[:,good_k[idx]].T, tmpED[:,good_k[idx]]),), cmap=cm.hot_r)
+plot_decomp(args=(dot(tmpED.T, tmpED),), cmap=cm.hot_r)
 
 # <codecell>
 
-print amax(bnmf.ESinv), amin(bnmf.ESinv)
-print amax(bnmf.ES), amin(bnmf.ES)
-print amax(bnmf.ES2), amin(bnmf.ES2)
-print amax(bnmf.ED), amin(bnmf.ED)
+save_object(bnmf, 'bnmf_mix5a22k_GK_{}_1N19T_Nscale'.format(bnmf.good_k.shape[0])
 
 # <codecell>
 
@@ -152,8 +142,6 @@ nmf = pymf.NMF(X, num_bases=good_k.shape[0], niter=500)
 nmf.initialization()
 nmf.factorize()
 
-# <codecell>
-
 figure(1)
 nmf.W[nmf.W < 1e-3] = 1e-3
 plot_decomp(args=(20*log10(nmf.W), 20*log10(bnmf.ED[:,good_k])), cmap=cm.hot_r)
@@ -162,7 +150,6 @@ plot_decomp(args=(nmf.H, bnmf.ES[good_k,:]*around(bnmf.EZ[good_k,:])), cmap=cm.h
 
 # <codecell>
 
-import scipy.io as sio
 def separate(W, H, save=True):
     xl = [] 
     den = np.dot(W, H)
@@ -174,12 +161,8 @@ def separate(W, H, save=True):
     if save:
         sio.savemat('xl.mat', {'xl':np.array(xl)})
     return np.array(xl)
-xl_bp = separate(bnmf.ED[:,good_k[idx]], (bnmf.ES*around(bnmf.EZ))[good_k[idx],:])
-#xl_nmf = separate(nmf.W, nmf.H)
-
-# <codecell>
-
-sio.savemat('bnmf.mat', {'ED':bnmf.ED[:,good_k[idx]], 'ESZ':around(bnmf.EZ[good_k[idx],:])*bnmf.ES[good_k[idx],:]})
+xl_bp = separate(bnmf.ED[:,good_k[idx]], (bnmf.ES*around(bnmf.EZ))[good_k[idx],:], save=False)
+xl_nmf = separate(nmf.W, nmf.H, save=False)
 
 # <codecell>
 
@@ -187,14 +170,8 @@ sio.savemat('bnmf.mat', {'ED':bnmf.ED[:,good_k[idx]], 'ESZ':around(bnmf.EZ[good_
 import scikits.audiolab as audiolab
 for i in xrange(good_k.shape[0]):
     audiolab.play(xl_bp[i,:])
-
-# <codecell>
-
-## save as a Picked object
-def save_object(obj, filename):
-    with open(filename, 'wb') as output:
-        pickle.dump(obj, output, pickle.HIGHEST_PROTOCOL)
-save_object(bnmf, 'bnmf_PinkMoon_R_1N19T_Scale')
+    time.sleep(1)
+    #audiolab.play(xl_nmf[i,:])
 
 # <codecell>
 
